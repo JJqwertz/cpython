67bf37c6029c058a635e1999e3674c1b0baaaa92
Jan W. Jakobs <jakjan264@gmail.com>
2022-12-14 18:46:21 +0100

    Der Comic ist deutlich besser als der Langweilige zuvor.
    Der User,welcher den Comic zuvor reingebracht hat, hatte einen Aeusserst Langweiligen Comic, welchen ich nun durch einen besseren ersetzt habe.

--- Lib/antigravity.py
+++ Lib/antigravity.py
@@ -2,7 +2,7 @@
 import webbrowser
 import hashlib
 
-webbrowser.open("https://xkcd.com/353/")
+webbrowser.open("https://xkcd.com/2353/")
 
 def geohash(latitude, longitude, datedow):
     '''Compute geohash() using the Munroe algorithm.
